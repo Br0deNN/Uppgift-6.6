0f44eea43e8d5b37044958e06bc6d4dd66f4834c
7R1XY <theo.broden@gmail.com>
2024-02-16 10:50:28 +0100

    Add project files.

---
+++ Uppgift 6.6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv in en mening");
+        string[] x = Console.ReadLine().Split(' ');
+        Uppercase(x);
+
+        Console.WriteLine();
+    }
+
+    static void Uppercase(string[] mening)
+    {
+        bool nextToUpper = true;
+
+        foreach (string ord in mening)
+        {
+            if (nextToUpper)
+            {
+                Console.Write(ord.ToUpper() + " ");
+            }
+            else
+            {
+                Console.Write(ord.ToLower() + " ");
+            }
+
+            nextToUpper = !nextToUpper;
+        }
+    }
+}
